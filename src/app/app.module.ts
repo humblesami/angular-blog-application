5b3e6dcb51838d237ae5ce070dcfa4dc94f0f3f1
abhinayjoshi <abhinayjoshi07@gmail.com>
2019-12-26 16:39:47 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BlogViewComponent } from './blog-view/blog-view.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, 
+     RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'products/:productId', component: BlogViewComponent },
+    ])
+  ],
   declarations: [ AppComponent, HomeComponent, BlogViewComponent ],
   bootstrap:    [ AppComponent ]
 })
